79225b89e47cdca1d5f39545b8cef65368d03f65
Caleb <37671933+cgmayfield@users.noreply.github.com>
2020-10-12 21:23:41 -0500

    Add menu to intro level

---
+++ proj/Unity/Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour {
+
+    public void Level1 ()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 2));
+    }
+
+    public void Level2 ()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void Level3 ()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+
+    public void Level4 ()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
+    }
+
+    public void Level5 ()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 6);
+    }
+
+    public IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play animation
+        //transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(1);
+
+        //Load scene
+        SceneManager.LoadScene(levelIndex);
+    }
+
+}
---
+++ proj/Unity/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour {
+
+    public void Back ()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex - 2));
+        Debug.Log("Back");
+    }
+
+    public IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play animation
+        //transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(1);
+
+        //Load scene
+        SceneManager.LoadScene(levelIndex);
+    }
+
+}
