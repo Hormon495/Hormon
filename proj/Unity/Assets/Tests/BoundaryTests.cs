d5cd6467599dffb7d87505b4d39a5311c89dbf14
dlwar3 <dlwarren2@crimson.ua.edu>
2020-11-10 01:18:52 -0600

    Did the exact same thing for spine

--- proj/Unity/Assets/Tests/BoundaryTests.cs
+++ proj/Unity/Assets/Tests/BoundaryTests.cs
@@ -49,4 +49,33 @@ namespace Tests
 
             Assert.IsTrue(body.isGrounded);
         }
+
+
+        [UnityTest]
+        public IEnumerator SpineCollision()
+        {
+            var gameObject = new GameObject();
+            edgeCollider2D = gameObject.AddComponent<EdgeCollider2D>();
+            List<Vector2> points = new List<Vector2> {
+                new Vector2(-0.42338628f, -0.33771434f), new Vector2(-0.27438346f, -0.27500242f), new Vector2(-0.2181394f, -0.21217895f), new Vector2(-0.22508667f, -0.07866065f), new Vector2(-0.20784585f, -0.048826963f), new Vector2(-0.17483893f, -0.029122554f), new Vector2(-0.19026527f, 0.0075134914f), new Vector2(-0.16010085f, 0.10201358f),
+                new Vector2(-0.11279889f, 0.1204476f), new Vector2(-0.09608494f, 0.17076856f), new Vector2(-0.05489932f, 0.35008773f), new Vector2(-0.037464887f, 0.35224938f), new Vector2(-0.03552404f, 0.39520073f), new Vector2(-0.018119633f, 0.40756997f), new Vector2(-0.032724537f, 0.44011682f), new Vector2(-0.005022198f, 0.51748574f),
+                new Vector2(0.020055555f, 0.5197035f), new Vector2(0.017560877f, 0.54796743f), new Vector2(0.051633097f, 0.55458343f), new Vector2(0.052665137f, 0.61078185f), new Vector2(0.07225179f, 0.6560588f), new Vector2(0.083806925f, 0.7079313f), new Vector2(0.07909122f, 0.7679362f), new Vector2(0.097843595f, 0.7684508f),
+                new Vector2(0.10023857f, 0.8233896f), new Vector2(0.09754634f, 0.8807752f), new Vector2(0.11253804f, 0.8856836f), new Vector2(0.11030191f, 0.919717f), new Vector2(0.13249327f, 0.94349885f), new Vector2(0.22289766f, 1.200381f), new Vector2(0.21521416f, 1.25941f), new Vector2(0.22004128f, 1.2962172f), new Vector2(0.22172804f, 1.3536625f), new Vector2(0.26825726f, 1.3819343f),
+                new Vector2(0.25668982f, 1.4386154f), new Vector2(0.2562974f, 1.5087137f), new Vector2(0.28076357f, 1.5828987f), new Vector2(0.30149722f, 1.6062098f), new Vector2(0.29584768f, 1.6441171f), new Vector2(0.29306418f, 1.714479f), new Vector2(0.32370237f, 1.7913812f), new Vector2(0.29623163f, 1.8702084f),
+                new Vector2(0.29460502f, 2.037129f), new Vector2(0.26148605f, 2.1153977f), new Vector2(0.26205605f, 2.1848114f), new Vector2(0.2750173f, 2.234324f), new Vector2(0.25557265f, 2.288236f), new Vector2(0.22420849f, 2.338448f), new Vector2(0.22109884f, 2.378279f), new Vector2(0.20219669f, 2.3951385f),
+                new Vector2(0.13814816f, 2.5204036f), new Vector2(0.15183538f, 2.542648f), new Vector2(0.0764623f, 2.6586378f), new Vector2(0.053137586f, 2.6713958f), new Vector2(0.06331987f, 2.709558f), new Vector2(0.03109657f, 2.7299592f), new Vector2(-0.021132883f, 2.8372211f), new Vector2(-0.030155722f, 2.9017007f),
+                new Vector2(-0.091898374f, 3.0338783f), new Vector2(-0.18492022f, 3.1098447f), new Vector2(-0.18594182f, 3.1573582f), new Vector2(-0.21100552f, 3.1561923f), new Vector2(-0.24149248f, 3.2130492f), new Vector2(-0.26501912f, 3.2738473f), new Vector2(-0.29456505f, 3.305498f), new Vector2(-0.3123181f, 3.347181f),
+                new Vector2(-0.3286137f, 3.3999684f), new Vector2(-0.32440424f, 3.474977f), new Vector2(-0.3258425f, 3.5172489f), new Vector2(-0.34285524f, 3.5190578f), new Vector2(-0.3463142f, 3.6322665f), new Vector2(-0.37661546f, 3.6697261f), new Vector2(-0.41834408f, 3.7224379f), new Vector2(-0.41724092f, 3.793549f),
+                new Vector2(-0.43248317f, 3.7999723f), new Vector2(-0.4494484f, 3.842824f), new Vector2(-0.4633612f, 3.8604496f), new Vector2(-0.4658556f, 3.8990784f), new Vector2(-0.4932365f, 3.9068506f), new Vector2(-0.4926115f, 3.9727147f), new Vector2(-0.5158235f, 4.050565f), new Vector2(-0.51110876f, 4.1050177f),
+                new Vector2(-0.5370481f, 4.1231995f), new Vector2(-0.5422942f, 4.192922f), new Vector2(-0.5741247f, 4.198603f), new Vector2(-0.60194224f, 4.354571f), new Vector2(-0.6270448f, 4.492844f), new Vector2(-0.63064253f, 4.593698f), new Vector2(-0.6147651f, 4.651174f), new Vector2(-0.61504054f, 4.7695136f),
+                new Vector2(-0.576317f, 4.930582f), new Vector2(-0.5747146f, 4.9980946f), new Vector2(-0.56183106f, 5.0311813f), new Vector2(-0.56533134f, 5.12007f), new Vector2(-0.5960586f, 5.260877f), new Vector2(-0.6414772f, 5.3357735f), new Vector2(-0.6393498f, 5.40661f)
+        };
+            edgeCollider2D.SetPoints(points);
+            edgeCollider2D.tag = "Ground";
+
+            yield return new WaitForSeconds(4);
+            Assert.IsTrue(body.isGrounded);
+            
+        }
+    }
 }
