a33f76ce2c0e125bdb2e4eba3ea6d67c7f49b650
cnkosek <cnkosek@crimson.ua.edu>
2020-12-03 01:44:17 -0600

    directional pad

---
+++ proj/Unity/Assets/Scripts/InputManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] TouchButton leftButton;
+    [SerializeField] TouchButton rightButton;
+    [SerializeField] TouchButton jumpButton;
+
+    public enum ButtonState
+    {
+        None,
+        PressedDown,
+        Released,
+        Held
+    }
+
+    public Vector2 CurrentInput
+    {
+        get
+        {
+            return new Vector2(HorizontalInput, VerticalInput);
+        }
+    }
+
+    float HorizontalInput
+    {
+        get
+        {
+            if (leftButton.CurrentState == ButtonState.Held || leftButton.CurrentState == ButtonState.PressedDown)
+            {
+                return -1;
+            }
+            else if (rightButton.CurrentState == ButtonState.Held || rightButton.CurrentState == ButtonState.PressedDown)
+            {
+                return 1;
+            }
+            return Input.GetAxis("Horizontal");
+        }
+    }
+
+    float VerticalInput
+    {
+        get
+        {
+            if (jumpButton.CurrentState == ButtonState.Held || jumpButton.CurrentState == ButtonState.PressedDown)
+            {
+                return 1;
+            }
+            return Input.GetAxis("Vertical");
+        }
+    }
+}
